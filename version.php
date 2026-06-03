82c10c8337a21a7a5336be8758b9c9c24b20ab32
Jordi <jordi@tresipunt.com>
2026-06-03 17:49:15 +0200

    Adaptation to moodle 5.x, fix errors in log pagination and add new get_category for idnumber WS

--- version.php
+++ version.php
@@ -33,7 +33,7 @@
  */
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2024061801;
+$plugin->version   = 2026052700;
 $plugin->requires  = 2021051703;
 $plugin->component = 'local_coursetransfer';
 $plugin->release   = '1.1.1';
